be4ac079a668dded4b7e1bbde359ea044e33fcee
ravimrai <ravimadhav1707@gmail.com>
2019-03-30 01:27:42 +0530

    Updates

---
+++ precache-manifest.745dda2f0fd4fc684c9142742477ac9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboapp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0bc66c940ef0fbae586c",
+    "url": "/roboapp/static/js/runtime~main.0bc66c94.js"
+  },
+  {
+    "revision": "0939601c5dab52723a76",
+    "url": "/roboapp/static/js/main.0939601c.chunk.js"
+  },
+  {
+    "revision": "bb30a3bcfa853b8d58db",
+    "url": "/roboapp/static/js/1.bb30a3bc.chunk.js"
+  },
+  {
+    "revision": "0939601c5dab52723a76",
+    "url": "/roboapp/static/css/main.61783f2f.chunk.css"
+  },
+  {
+    "revision": "bb30a3bcfa853b8d58db",
+    "url": "/roboapp/static/css/1.d56066dd.chunk.css"
+  },
+  {
+    "revision": "d48f7b5f03b5b17817e71dca189bcd7e",
+    "url": "/roboapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/musicalevent/precache-manifest.4d5aff66180d4295c4adb1949c586fe3.js"
+  "/roboapp/precache-manifest.745dda2f0fd4fc684c9142742477ac9a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/musicalevent/index.html", {
+workbox.routing.registerNavigationRoute("/roboapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
